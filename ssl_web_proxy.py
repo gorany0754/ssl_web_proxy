13980655e0d496d6db30d17837e6055fe9a7877c
gorany0754 <hoon0754@gmail.com>
2017-12-09 17:24:31 +0900

    Add sending established packet and wrap socket to ssl

--- ssl_web_proxy.py
+++ ssl_web_proxy.py
@@ -3,26 +3,23 @@ import os,sys,thread,socket,ssl
 MAX_QUEUE = 20          # Max number of connection
 MAX_PKT_SIZE = 99999     # Max size of packet
 
-def gen_cert(name):
-    cmd = 'cd cert-master && sh _make_site.sh '+ name
+def gen_cert(webserver):
+    cmd = 'cd cert-master && sh _make_site.sh '+ webserver
+    os.system(cmd)
+
+def init_cert():
+    cmd = 'cd cert-master && sh _init_site.sh '
     os.system(cmd)
 
 def main():
-    
-    # Usage  : python ssl_web_proxy.py
-    # Setting: set https proxy with localhost and port number 4433
-
     port = 4433
     host = ''
 
+    init_cert()
+
     try:
-        # create socket
         s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-        # bind host and port
         s.bind((host, port))
-
-        # listenning
         s.listen(MAX_QUEUE)
     
     except socket.error, (value, message):
@@ -31,11 +28,8 @@ def main():
         print "Fail to open socket"
         sys.exit(1)
 
-    # connect with web client 
     while 1:
         conn, client_addr = s.accept()
-
-        # thread to handle web client and end server
         thread.start_new_thread(proxy_thread, (conn, client_addr))
         
     s.close()
@@ -47,17 +41,13 @@ def proxy_thread(conn, client_addr):
     
     #Find CONNECT method and parsing 
     if request.find('CONNECT'):
-	# split first line 
 	line = request.split('\n')[0]
-	# get url
 	url = line.split(' ')[1]
-	# find the webserver and port
 	http_pos = url.find("://")          # find pos of ://
 	if (http_pos==-1):
 	    temp = url
 	else:
 	    temp = url[(http_pos+3):]       # get the rest of url
-	# find / in url and remove it
 	webserver_pos = temp.find("/")
 	if webserver_pos == -1:
 	    webserver_pos = len(temp)
@@ -65,39 +55,45 @@ def proxy_thread(conn, client_addr):
 	# host 
 	webserver = temp[:webserver_pos]
 	
+	# Connection established for hello
+	conn.send('HTTP/1.1 Connection established\r\n\r\n')
+
 	# generate certification
 	gen_cert(webserver)
+	
+	# define certification dir	
+	cert_dir= os.getcwd() +'/cert-master/'+ webserver + '.pem'
+	
+	# wrapping conn socket as ssl with cert
+	ssl_conn= ssl.wrap_socket(conn, server_side =True, keyfile = cert_dir, certfile = cert_dir)
+	
+	#receive ssl request
+	ssl_request = ssl_conn.recv(MAX_PKT_SIZE)
 
 	try:
-	    # create a socket to connect to the end server
+	    # create a ssl socket
 	    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-	    # connect end server with port 80
-	    s.connect((webserver, 80))
-	    # put dummy behind of request
-	    dummyRequest = 'GET / HTTP/1.1\r\nHost: test.gilgil.net\r\n\r\n'
-	    request= dummyRequest + request
+	    ssl_s = ssl.wrap_socket(s)
+	    # connect end server with port 443
+	    ssl_s.connect((webserver, 443))
+
 	    # send request to end server
-	    s.send(request)         
+	    ssl_s.send(ssl_request)         
 	    
 	    while 1:
 		# receive data from end server
-		data = s.recv(MAX_PKT_SIZE)
-		#print data
-		# if 404 data, ignore it
-		if data.find('HTTP/1.1 404 Not Found') >= 0:
-		    continue
+		data = ssl_s.recv(MAX_PKT_SIZE)
 		if (len(data) > 0):
 		    # send to browser
-		    conn.send(data)
+		    ssl_conn.send(data)
 		else:
 		    break
-		s.close()
-		conn.close()
+		ssl_s.close()
+		ssl_conn.close()
 	except socket.error, (value, message):
-	    s.close()
-	    conn.close()
+	    ssl_s.close()
+	    ssl_conn.close()
 	    sys.exit(1)
 
-    
 if __name__ == '__main__':
     main()
